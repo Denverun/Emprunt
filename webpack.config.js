efff22b59686bb882e75d510d67d0b1e312855e1
denverun <jacques.zhu@centrale-marseille.fr>
2022-05-28 08:26:10 +0200

    Partie 1

--- webpack.config.js
+++ webpack.config.js
@@ -21,7 +21,8 @@ Encore
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
     .addEntry('app', './assets/app.js')
-
+    .addEntry('formulaire', './assets/styles/formulaire.css')
+    .addEntry('ginfo', './assets/images/ginfo.png')
     // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
     .enableStimulusBridge('./assets/controllers.json')
 
